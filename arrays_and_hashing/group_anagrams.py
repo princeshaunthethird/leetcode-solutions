705e086b3f8232b7cd7599d4936855583bf9cb02
Shaun Dias <md263401@gmail.com>
2026-01-15 20:19:00 +0530

    Solve Group Anagrams using frequency hash map

---
+++ arrays_and_hashing/group_anagrams.py
@@ -0,0 +1,15 @@
+from collections import defaultdict
+from typing import List
+
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        groups = defaultdict(list)
+
+        for word in strs:
+            count = [0] * 26
+            for c in word:
+                count[ord(c) - ord('a')] += 1
+
+            groups[tuple(count)].append(word)
+
+        return list(groups.values())
