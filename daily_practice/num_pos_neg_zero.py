2ef2bf14a7b7e7fb50e1500a36c49956dad7d4cd
Shaun Dias <md263401@gmail.com>
2026-01-23 19:05:13 +0530

    check the num is positive , negative or zero

---
+++ daily_practice/num_pos_neg_zero.py
@@ -0,0 +1,9 @@
+num = float(input("Enter the number you wanna test"))
+
+if num == 0:
+    print(f"the number {num} is zero")
+elif num > 0:
+    print(f"the number {num} is a positive number")
+else:
+    print(f"The number {num} is a negative number")
+    
