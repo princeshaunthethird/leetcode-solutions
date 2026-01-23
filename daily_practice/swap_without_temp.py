7a0a0c511e1fac33d320ac453b1966eb27195c05
Shaun Dias <md263401@gmail.com>
2026-01-23 18:55:58 +0530

    add swap without temp program

---
+++ daily_practice/swap_without_temp.py
@@ -0,0 +1,5 @@
+a = float(input("Enter the first number"))
+b = float(input("Enter the seconde number"))
+print(f"before swapping the numbers are {a} and {b}")
+a, b = b, a
+print(f"after swapping the numbers are {a} and {b}")
