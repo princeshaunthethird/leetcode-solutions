c2a988eb21fff764f9b040a6ac391ca16a2c8637
Shaun Dias <md263401@gmail.com>
2026-01-24 18:12:45 +0530

    Updated calander.py file

--- daily_practice/calander.py
+++ daily_practice/calander.py
@@ -4,4 +4,5 @@ year = int(input("ENTER THE YEAR"))
 month = int (input("Enter the month u need"))
 ca = calendar.month(year,month)
 print(ca)
+# Print the full calendar for 2024
 calendar.prcal(2026)
