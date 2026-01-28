2b0fb680635f099258f9e509aaaf2365fae92b82
Shaun Dias <md263401@gmail.com>
2026-01-29 01:10:40 +0530

    simple conversion

--- daily_practice/convert_decimal_to_many_other.py
+++ daily_practice/convert_decimal_to_many_other.py
@@ -1,5 +1,5 @@
 dec_num = int(input('Enter a decimal number: '))
-print("The decimal value of", dec_num, "is:")
+print(f"The decimal value of {dec_num} is:")
 print(bin(dec_num), "in binary.")
 print(oct(dec_num), "in octal.")
 print(hex(dec_num), "in hexadecimal.")
